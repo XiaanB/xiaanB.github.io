f3c0c31eb94621229ffdb06611f70a5e55c548d0
XiaanB <xiaan_b@hotmail.com>
2023-09-14 06:56:04 +1200

    testing mailchimp

--- ContactJava.js
+++ ContactJava.js
@@ -1,15 +1,15 @@
-function OnSubmitForm()
-{
-  var a = document.myform1.getElementsById('fname');
-  if(a == "cb" ){
-    document.myform1.action ="Tours.html#NI";
-    // && object.budget[3].checked == true
-  }
-else 
-	windows.alert(10);
-  // else if (document.myform.Destination[1].checked == true) {
-  //   document.myform.action ="Tours.html#SI";
-  // }
-  // else
-  //   document.myform.action ="Tours.html#BI"; 
-}
+// function OnSubmitForm()
+// {
+//   var a = document.myform1.getElementsById('fname');
+//   if(a == "cb" ){
+//     document.myform1.action ="Tours.html#NI";
+//     // && object.budget[3].checked == true
+//   }
+// else 
+// 	windows.alert(10);
+//   // else if (document.myform.Destination[1].checked == true) {
+//   //   document.myform.action ="Tours.html#SI";
+//   // }
+//   // else
+//   //   document.myform.action ="Tours.html#BI"; 
+// }
