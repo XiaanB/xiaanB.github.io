14e6e02c44f21aec371b0263344c9c41c8a80e4b
XiaanB <xiaan_b@hotmail.com>
2023-09-17 10:47:21 +1200

    Updating contact form with dom manupilation.,

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,3 +4,16 @@ if(4+3 ===7){
 	// alert("You are smart.");
 	console.log("Helooooooo");
 }
+
+function sayHello(){
+	console.log("Hola");
+}
+
+sayHello();
+sayHello();
+
+var sayBye = function(){
+	console.log("Bye");
+}
+
+sayBye();
